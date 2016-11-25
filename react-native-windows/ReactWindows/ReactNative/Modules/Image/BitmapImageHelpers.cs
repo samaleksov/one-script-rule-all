a8a59b7c6ad99c361607faa63bafcd0b5f482bd3
Samuil Aleksov <Samuil_Aleksov@epam.com>
2016-11-25 14:37:17 +0200

    adding react native windows again

---
+++ react-native-windows/ReactWindows/ReactNative/Modules/Image/BitmapImageHelpers.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+using static System.FormattableString;
+
+namespace ReactNative.Modules.Image
+{
+    static class BitmapImageHelpers
+    {
+        public static bool IsBase64Uri(string uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            return uri.StartsWith("data:");
+        }
+
+        public static async Task<IRandomAccessStream> GetStreamAsync(string uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            if (IsBase64Uri(uri))
+            {
+                var decodedData = Convert.FromBase64String(uri.Substring(uri.IndexOf(",") + 1));
+                return decodedData.AsBuffer().AsStream().AsRandomAccessStream();
+            }
+            else
+            {
+                var uriValue = default(Uri);
+                if (!Uri.TryCreate(uri, UriKind.Absolute, out uriValue))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(uri), Invariant($"Invalid URI '{uri}' provided."));
+                }
+
+                var streamReference = RandomAccessStreamReference.CreateFromUri(uriValue);
+                return await streamReference.OpenReadAsync();
+            }
+        }
+
+        public static IObservable<ImageStatusEventData> GetStreamLoadObservable(this BitmapImage image)
+        {
+            return image.GetOpenedObservable()
+                .Merge(image.GetFailedObservable(), Scheduler.Default)
+                .StartWith(new ImageStatusEventData(ImageLoadStatus.OnLoadStart));
+        }
+  
+        private static IObservable<ImageStatusEventData> GetOpenedObservable(this BitmapImage image)
+        {
+            return Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
+                h => image.ImageOpened += h,
+                h => image.ImageOpened -= h)
+                .Select(args =>
+                {
+                    var bitmapImage = args.Sender as BitmapImage;
+                    if (bitmapImage != null)
+                    {
+                        return new ImageStatusEventData(
+                            ImageLoadStatus.OnLoad,
+                            new ImageMetadata(
+                                image.UriSource?.AbsoluteUri,
+                                image.PixelWidth,
+                                image.PixelHeight));
+                    }
+                    else
+                    {
+                        return new ImageStatusEventData(ImageLoadStatus.OnLoad);
+                    }
+                })
+                .Take(1)
+                .Concat(Observable.Return(new ImageStatusEventData(ImageLoadStatus.OnLoadEnd)));
+        }
+
+        private static IObservable<ImageStatusEventData> GetFailedObservable(this BitmapImage image)
+        {
+            return Observable.FromEventPattern<ExceptionRoutedEventHandler, ExceptionRoutedEventArgs>(
+                h => image.ImageFailed += h,
+                h => image.ImageFailed -= h)
+                .Select<EventPattern<ExceptionRoutedEventArgs>, ImageStatusEventData>(pattern =>
+                {
+                    throw new InvalidOperationException(pattern.EventArgs.ErrorMessage);
+                });
+        }
+    }
+}
---
+++ react-native-windows/ReactWindows/ReactNative/Views/Text/ReactTextCompoundView.cs
@@ -0,0 +1,17 @@
+﻿using ReactNative.UIManager;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ReactNative.Views.Text
+{
+    class ReactTextCompoundView : IReactCompoundView
+    {
+        public int GetReactTagAtPoint(UIElement reactView, Point point)
+        {
+            var richTextBlock = reactView.As<RichTextBlock>();
+            var textPointer = richTextBlock.GetPositionFromPoint(point);
+            return textPointer.Parent.GetTag();
+        }
+    }
+}
